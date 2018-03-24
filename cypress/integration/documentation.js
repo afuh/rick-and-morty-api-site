4ebb9a52c9564655f2845fb8531f9c926a34c62b
Axel <axelfuh@gmail.com>
2018-03-24 12:01:53 +0100

    Update tests

--- cypress/integration/documentation.js
+++ cypress/integration/documentation.js
@@ -1,6 +1,11 @@
-/* global it describe cy before*/
+/* global it describe cy before expect*/
 import config from '../../config/SiteConfig';
 const YAML = require('yamljs')
+
+const libraries = [
+  { id: 'GraphQL', link: 'https://github.com/arthurdenner/rick-and-morty-graphql-api', author: 'https://github.com/arthurdenner' },
+  { id: 'Ruby', link: 'https://github.com/spielhoelle/rick-and-morty-gem', author: 'https://github.com/spielhoelle' }
+]
 
 describe("Documentation page", () => {
   before(() => {
@@ -35,6 +40,14 @@ describe("Documentation page", () => {
       cy.get('aside').should('have.css', 'position').and('match', /fixed/)
     })
 
+    it("Should become a scrolleable sidebar", () => {
+      cy.viewport(1200, 600)
+      cy.scrollTo('bottom')
+      cy.get('aside').scrollTo('bottom')
+      cy.wait(500)
+      cy.get('aside').scrollTo('top')
+    })
+
     it("Should be an index with Headers and links", () => {
       cy.readFile('/src/data/docs-index.yaml')
         .then(index => {
@@ -51,6 +64,23 @@ describe("Documentation page", () => {
         })
       })
 
+  })
+
+  describe('Libraries section', () => {
+    it("Should have a Title and two working links", () => {
+      for (const key in libraries) {
+        cy.get('article').within(() => {
+          cy.get('h4').contains(libraries[key].id)
+
+          cy.get(`a[href='${libraries[key].link}']`)
+          cy.request(libraries[key].link).then(res => expect(res.status).not.to.eq(400))
+
+          cy.get(`a[href='${libraries[key].author}']`)
+          cy.request(libraries[key].author).then(res => expect(res.status).not.to.eq(400))
+
+        })
+      }
+    })
   })
 
   describe('Github edit page button', () => {
