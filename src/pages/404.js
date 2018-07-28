fac9e284da1e3130db285b0fe512dd79a8065bbe
Axel <axelfuh@gmail.com>
2018-07-28 16:56:23 +0200

    Refactor 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,13 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import config from '../../config/SiteConfig'
-import Error from '../components/Error/Error'
+import ErrorMessage from '../components/Error/Error'
 import Layout from '../components/Layout'
 
 const NotFoundPage = ({ location }) => (
   <Layout location={location}>
-    <div>
-      <Helmet title={`Oh Jeez! | ${config.siteTitle}`} />
-      <Error message='Oh Jeez! there is nothing here.'/>
-    </div>
+    <Helmet title={`Oh Jeez! | ${config.siteTitle}`} />
+    <ErrorMessage />
   </Layout>
 )
 
