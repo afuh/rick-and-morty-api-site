f16b7a51ab1fdd6d4a135d42f9395bb90622c0a7
Axel <axelfuh@gmail.com>
2018-07-29 09:46:13 +0200

    Lint

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,23 +10,23 @@ const path = require(`path`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-   const slug = createFilePath({ node, getNode, basePath: `pages` })
-   createNodeField({
-     node,
-     name: `slug`,
-     value: slug,
-   })
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
   }
 }
 
 exports.createPages = ({ graphql, actions }) => {
-	const { createPage } = actions
+  const { createPage } = actions
 
-	return new Promise((resolve, reject) => {
-		const component = path.resolve('src/templates/markdown.js')
-		resolve(
-			graphql(
-				`
+  return new Promise((resolve, reject) => {
+    const component = path.resolve('src/templates/markdown.js')
+    resolve(
+      graphql(
+        `
         {
           projects: allMarkdownRemark {
             edges {
@@ -39,22 +39,22 @@ exports.createPages = ({ graphql, actions }) => {
           }
         }
       `
-			).then(result => {
-				if (result.errors) {
-					console.log(result.errors)
-					reject(result.errors)
-				}
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
 
-				result.data.projects.edges.forEach(edge => {
-					createPage({
-						path: edge.node.fields.slug,
-						component,
-						context: {
-							slug: edge.node.fields.slug,
-						}
-					})
-				})
-			})
-		)
-	})
+        result.data.projects.edges.forEach(edge => {
+          createPage({
+            path: edge.node.fields.slug,
+            component,
+            context: {
+              slug: edge.node.fields.slug
+            }
+          })
+        })
+      })
+    )
+  })
 }
