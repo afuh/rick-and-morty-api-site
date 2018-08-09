33e4dc2c1489164e8c113182d54c8f33d86303c1
Axel <axelfuh@gmail.com>
2018-08-09 18:41:46 +0200

    Update style utils

--- src/styles/utils.js
+++ src/styles/utils.js
@@ -7,6 +7,34 @@ export const rem = (...arg) => _rem(...arg)
 
 export const navHeight = _rem(80)
 
+export const media = {
+  lg: inner => css`
+    @media (max-width: ${1200 / 16}em) {
+      ${inner}
+    }
+  `,
+  md: inner => css`
+    @media (max-width: ${992 / 16}em) {
+      ${inner}
+    }
+  `,
+  sm: inner => css`
+    @media (max-width: ${768 / 16}em) {
+      ${inner}
+    }
+  `,
+  xs: inner => css`
+    @media (max-width: ${480 / 16}em) {
+      ${inner}
+    }
+  `,
+  custom: (n, inner) => css`
+    @media (max-width: ${n / 16}em) {
+      ${inner}
+    }
+  `
+}
+
 export const mobile = inner => css`
   @media (max-width: ${1000 / 16}em) {
     ${inner}
@@ -27,8 +55,8 @@ export const centrate = css`
 
 export const flex = inner => css`
   display: flex;
-  justify-content: ${inner.x};
-  align-items: ${inner.y};
+  justify-content: ${inner.x || 'center'};
+  align-items: ${inner.y || 'center'};
 `
 
 export const hover = inner => css`
@@ -42,6 +70,7 @@ export const theme = {
   orange: '#FF9800',
   black: '#333',
   backBlack: '#202329',
+  lightgray: '#d8d8d8',
   border: '1px solid #f4f5fA',
   shadow: '0 1px 6px 0 #e2e4ed'
 }
