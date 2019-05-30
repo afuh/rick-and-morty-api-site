9b62db6f1ce9f117ec0864bdeb7830b8010cfe92
Axel <axelfuh@gmail.com>
2019-05-30 13:29:47 +0200

    Add sidebar hook

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,3 +1,4 @@
 export { useSiteMeta } from './useSiteMeta'
 export { useRickAndMortyStats } from './useRickAndMortyStats'
 export { useRandomChars } from './useRandomChars'
+export { useSidebar } from './useSidebar'
