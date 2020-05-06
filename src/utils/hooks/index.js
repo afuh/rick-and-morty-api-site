9f384d3e8cbf5475f49ed7d3fad5bd47c0d60549
Axel <axelfuh@gmail.com>
2020-05-06 18:30:37 +0200

    remove unused hook

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,6 +1,5 @@
 export { useSiteMeta } from './useSiteMeta'
 export { useRickAndMortyStats } from './useRickAndMortyStats'
 export { useRandomChars } from './useRandomChars'
-export { useStickySidebar } from './useStickySidebar'
 export { useMobileSidebar } from './useMobileSidebar'
 export { useServerStatus } from './useServerStatus'
