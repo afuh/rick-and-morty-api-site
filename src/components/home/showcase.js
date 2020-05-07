c0897beb223db5fe805b9dfb38accdccc3915960
Axel <axelfuh@gmail.com>
2020-05-07 08:14:24 +0200

    add custom spinner

--- src/components/home/showcase.js
+++ src/components/home/showcase.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-import Spinner from 'react-spinkit'
 
 import { useRandomChars } from '../../utils/hooks'
 import Card from './characterCard'
+import { Spinner } from '../shared'
 
 const Wrapper = styled.section`
   ${({ theme }) => css`
     ${theme.mixins.flex}
-
     padding: ${theme.spacing._32};
-    background: ${theme.backBlack};
-    min-height: calc(60vh - ${theme.navHeight}px);
+    background: ${theme.lightgray};
+    min-height: calc(50vh - ${theme.navHeight}px);
 
     ${theme.media.phone(css`
       padding: 0;
@@ -22,35 +21,17 @@ const Wrapper = styled.section`
 const Inner = styled.div`
   display: flex;
   justify-content: space-around;
+  align-content: center;
   flex-wrap: wrap;
   max-width: 1260px;
-  min-height: 40vh;
 `
-
-const Loading = () => (
-  <Spinner
-    name='triangle-skew-spin'
-    color='rgb(255, 152, 0)'
-  />
-)
 
 const Showcase = () => {
   const { loading, data } = useRandomChars({ total: 8 })
 
   return (
     <Wrapper>
-      <Inner>
-        {
-          loading ?
-            <Loading /> :
-            data.map(char => (
-              <Card
-                key={char.id}
-                char={char}
-              />
-            ))
-        }
-      </Inner>
+      <Inner>{loading ? <Spinner /> : data.map((char) => <Card key={char.id} char={char} />)}</Inner>
     </Wrapper>
   )
 }
