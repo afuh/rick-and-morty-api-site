c7e4572decb000315cbd65eab54e0508210a4ea9
Axel Fuhrmann <me@afuh.dev>
2026-01-24 11:39:11 +0100

    Switch Netlify proxy redirects from 200 to 308 (#93)
    * fix: update redirect status to 308
    
    * chore: add .nvmrc file and update about page content
    
    * chore: comment out e2e test

--- cypress/integration/documentation.js
+++ cypress/integration/documentation.js
@@ -1,23 +1,5 @@
 import config from '../../config/siteConfig'
 
-const externalLinks = [
-  'https://github.com/spielhoelle/rick-and-morty-gem',
-  'https://github.com/l1h3r/ex_shla',
-  'https://github.com/curiousrohan/ramapi',
-  'https://github.com/Carlj28/RickAndMorty.Net.Api',
-  'https://github.com/pitakill/rickandmortyapigowrapper',
-  'https://github.com/MikeJohnPage/mortyr',
-  'https://github.com/adrianoluis/rickandmortyapi-java',
-  'https://github.com/benjaminbruch/rick-morty-swift-api',
-  'https://github.com/dshomoye/rick-and-morty',
-  'https://github.com/bigdummyhead/rick.net',
-  'https://github.com/loopDelicious/rick-and-morty-postman',
-  'https://github.com/Yash-Garg/RickandMorty-Dart-Wrapper',
-  'https://github.com/afuh/rick-and-morty-api-node',
-  'https://javascript.rickandmortyapi.com',
-  'https://github.com/nickbeen/rick-and-morty-api-php',
-]
-
 describe('Documentation page', () => {
   before(() => {
     cy.visit('/documentation')
@@ -55,20 +37,6 @@ describe('Documentation page', () => {
   describe('Github edit page button', () => {
     it('Should be a link to edit the page', () => {
       cy.get('a.edit-page').should('have.attr', 'href', config.github.site + '/blob/master/src/pages/documentation.mdx')
-    })
-  })
-
-  describe('External links. ', () => {
-    externalLinks.forEach((item) => {
-      const name = item.split('/').pop()
-
-      it(name, () => {
-        cy.get('main').within(() => {
-          cy.get(`a[href='${item}']`)
-            .request(item)
-            .then((res) => expect(res.status).to.eq(200))
-        })
-      })
     })
   })
 })
