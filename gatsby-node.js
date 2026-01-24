dcd11f73e467a1edc5411ce1ab0a86abf1d9e76e
Axel Fuhrmann <me@afuh.dev>
2026-01-24 15:38:43 +0100

    redirects (#94)
    * chore: remove redirects
    
    * fix: update GraphQL API URL to use environment variable

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,19 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 
-exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions
+// exports.createPages = ({ actions }) => {
+//   const { createRedirect } = actions
 
-  createRedirect({
-    fromPath: '/api/*',
-    toPath: `${process.env.REST_API_URL}/:splat`,
-    statusCode: 308,
-  })
+//   createRedirect({
+//     fromPath: '/api/*',
+//     toPath: `${process.env.REST_API_URL}/:splat`,
+//     statusCode: 308,
+//   })
 
-  createRedirect({
-    fromPath: '/graphql',
-    toPath: `${process.env.GRAPHQL_API_URL}`,
-    statusCode: 308,
-  })
-}
+//   createRedirect({
+//     fromPath: '/graphql',
+//     toPath: `${process.env.GRAPHQL_API_URL}`,
+//     statusCode: 308,
+//   })
+// }
